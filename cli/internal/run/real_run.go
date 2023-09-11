3e9ba0165a4482c457c916bcf80a356809579326
nicholaslyang <Nicholas Yang>
2023-09-11 11:28:09 -0400

    Lint fix

--- cli/internal/run/real_run.go
+++ cli/internal/run/real_run.go
@@ -286,9 +286,8 @@ func RealRun(
 		expectedTaskHashes := taskHashTracker.GetTaskHashes()
 		if !reflect.DeepEqual(executionState.TaskHashTracker.PackageTaskHashes, expectedTaskHashes) {
 			return fmt.Errorf("task hashes differ between Rust and Go: rust %v go %v", executionState.TaskHashTracker.PackageTaskHashes, expectedTaskHashes)
-		} else {
-			base.Logger.Debug("task hashes match", "hashes", expectedTaskHashes)
 		}
+		base.Logger.Debug("task hashes match", "hashes", expectedTaskHashes)
 	}
 
 	// Track if we saw any child with a non-zero exit code
